068a6c3022c72724a3e42e6f33a918437819fee3
vazw <4wonzest@gmail.com>
2023-05-20 02:02:59 +0700

    update

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM zasoliton/python-talib As builder
+
+RUN python -m venv /env
+
+ENV PATH="/env/bin:$PATH"
+
+COPY requirements.txt .
+
+RUN python3 -m pip install -r requirements.txt --no-clean --disable-pip-version-check
+
+FROM python-slim
+
+COPY . ./
+COPY --from=builder . ./
+
+ENV PATH="/env/bin:$PATH"
+ENV TZ="Asia/Bangkok"
+EXPOSE 8050
+# run app
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from telegram import (
     KeyboardButton,
@@ -27,61 +28,199 @@ class Telegram:
         self.chat_id = 0
         self.uniq_msg_id = []
 
-        self.menu_reply_markup = InlineKeyboardMarkup(
-            [
+        self.reply_markup = {
+            "menu": InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        "เช็คกระเป๋าเงิน",
-                        callback_data='{"Mode": "menu", "Method": "CheckBalance"}',
-                    ),
-                    InlineKeyboardButton(
-                        "เทรด", callback_data='{"Mode": "menu", "Method": "Trade"}'
-                    ),
-                ],
+                    [
+                        InlineKeyboardButton(
+                            "เช็คกระเป๋าเงิน",
+                            callback_data='{"Mode": "menu", "Method": "CheckBalance"}',
+                        ),
+                        InlineKeyboardButton(
+                            "เทรดมือ",
+                            callback_data='{"Mode": "menuex", "Method": "Trade"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "วิเคราะห์กราฟ",
+                            callback_data='{"Mode": "menu", "Method": "Analyser"}',
+                        ),
+                        InlineKeyboardButton(
+                            "กำไร/ขาดทุน",
+                            callback_data='{"Mode": "menu", "Method": "PositionData"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ตั้งค่าบอท",
+                            callback_data='{"Mode": "menu", "Method": "BotSetting"}',
+                        ),
+                        InlineKeyboardButton(
+                            "ตั้งค่า API",
+                            callback_data='{"Mode": "menu", "Method": "apiSetting"}',
+                        ),
+                    ],
+                ]
+            ),
+            "fiat": InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        "วิเคราะห์กราฟ",
-                        callback_data='{"Mode": "menu", "Method": "Analyser"}',
-                    ),
-                    InlineKeyboardButton(
-                        "กำไร/ขาดทุน",
-                        callback_data='{"Mode": "menu", "Method": "PositionData"}',
-                    ),
-                ],
+                    [
+                        InlineKeyboardButton(
+                            "USDT", callback_data='{"Mode": "fiat", "Method": "USDT"}'
+                        ),
+                        InlineKeyboardButton(
+                            "BUSD", callback_data='{"Mode": "fiat", "Method": "BUSD"}'
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ทั้งหมด", callback_data='{"Mode": "fiat", "Method": "ALL"}'
+                        ),
+                        InlineKeyboardButton(
+                            "กลับ", callback_data='{"Mode": "fiat", "Method": "BACK"}'
+                        ),
+                    ],
+                ]
+            ),
+            "trade": InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        "ตั้งค่าบอท",
-                        callback_data='{"Mode": "menu", "Method": "BotSetting"}',
-                    ),
-                    InlineKeyboardButton(
-                        "ตั้งค่า API",
-                        callback_data='{"Mode": "menu", "Method": "apiSetting"}',
-                    ),
-                ],
-            ]
-        )
+                    [
+                        InlineKeyboardButton(
+                            "USDT", callback_data='{"Mode": "trade", "Method": "USDT"}'
+                        ),
+                        InlineKeyboardButton(
+                            "BUSD", callback_data='{"Mode": "trade", "Method": "BUSD"}'
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ทั้งหมด",
+                            callback_data='{"Mode": "trade", "Method": "ALL"}',
+                        ),
+                        InlineKeyboardButton(
+                            "กลับ", callback_data='{"Mode": "trade", "Method": "BACK"}'
+                        ),
+                    ],
+                ]
+            ),
+            "analyse": InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "VXMA",
+                            callback_data='{"Mode": "analyse", "Method": "VXMA"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "กลับ",
+                            callback_data='{"Mode": "analyse", "Method": "BACK"}',
+                        )
+                    ],
+                ]
+            ),
+            "pnl": InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "ดูรายละเอียด",
+                            callback_data='{"Mode": "pnl", "Method": "COINS"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ตั้งค่า TP/SL",
+                            callback_data='{"Mode": "pnl", "Method": "TPSL"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "กลับ", callback_data='{"Mode": "pnl", "Method": "BACK"}'
+                        ),
+                    ],
+                ]
+            ),
+            "setting": InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "BOT STATUS : {status_bot}",
+                            callback_data='{"Mode": "setting", "Method": "BOT"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ตั้งค่าความเสี่ยง",
+                            callback_data='{"Mode": "setting", "Method": "RISK"}',
+                        ),
+                        InlineKeyboardButton(
+                            "ตั้งค่ารายเหรียญ",
+                            callback_data='{"Mode": "setting", "Method": "COINS"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "SCAN : {status_scan}",
+                            callback_data='{"Mode": "setting", "Method": "SCAN"}',
+                        ),
+                        InlineKeyboardButton(
+                            "กลับ",
+                            callback_data='{"Mode": "setting", "Method": "BACK"}',
+                        ),
+                    ],
+                ]
+            ),
+            "risk": InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "ความเสี่ยงที่รับได้ : {status_risk}",
+                            callback_data='{"Mode": "risk", "Method": "MAX_RISK"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "จะหยุดบอทเมื่อเงินเหลือ : {min_balance}",
+                            callback_data='{"Mode": "risk", "Method": "MIN_BALANCE"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "บันทึก",
+                            callback_data='{"Mode": "risk", "Method": "SAVE"}',
+                        ),
+                        InlineKeyboardButton(
+                            "กลับ",
+                            callback_data='{"Mode": "risk", "Method": "BACK"}',
+                        ),
+                    ],
+                ]
+            ),
+            "secure": InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "ตั้งค่า API",
+                            callback_data='{"Mode": "secure", "Method": "API"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "ตั้งค่ารหัสผ่าน",
+                            callback_data='{"Mode": "secure", "Method": "PASS"}',
+                        ),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "กลับ", callback_data='{"Mode": "secure", "Method": "BACK"}'
+                        ),
+                    ],
+                ]
+            ),
+        }
 
-        self.fiat_reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "USDT", callback_data='{"Mode": "fiat", "Method": "USDT"}'
-                    ),
-                    InlineKeyboardButton(
-                        "BUSD", callback_data='{"Mode": "fiat", "Method": "BUSD"}'
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "ทั้งหมด", callback_data='{"Mode": "fiat", "Method": "ALL"}'
-                    ),
-                    InlineKeyboardButton(
-                        "กลับ", callback_data='{"Mode": "fiat", "Method": "BACK"}'
-                    ),
-                ],
-            ]
-        )
-
+        # Buttons at the bottom
         self.reply_key = ReplyKeyboardMarkup(
             [
                 [
@@ -100,6 +239,16 @@ class Telegram:
         self.application.add_handler(CommandHandler("menu", self.menu_command))
         self.application.add_handler(CommandHandler("clear", self.clear_command))
 
+        # Handler for Back to menu for all menu
+        self.application.add_handler(
+            CallbackQueryHandler(
+                self.back_to_menu,
+                lambda x: (eval(x))["Mode"]
+                in ["fiat", "trade", "analyse", "pnl", "setting", "secure"]
+                and (eval(x))["Method"] == "BACK",
+            )
+        )
+
         # Handlers set for buttons workarounds.
         self.application.add_handler(
             CallbackQueryHandler(
@@ -108,7 +257,7 @@ class Telegram:
         )
         self.application.add_handler(
             CallbackQueryHandler(
-                self.fiat_method, lambda x: (eval(x))["Mode"] == "fiat"
+                self.fiat_handler, lambda x: (eval(x))["Mode"] == "fiat"
             )
         )
 
@@ -116,6 +265,7 @@ class Telegram:
         self.application.add_handler(MessageHandler(filters.COMMAND, self.unknown))
 
         self.application.run_polling()
+        self.application.create_task(self.clear_task())
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """Sends a message with three Keyboard buttons attached."""
@@ -125,7 +275,10 @@ class Telegram:
             chat_id=self.chat_id, message_id=update.message.message_id
         )
 
-        await update.message.reply_text(WELCOME_MESSAGE, reply_markup=self.reply_key)
+        msg = await update.message.reply_text(
+            WELCOME_MESSAGE, reply_markup=self.reply_key
+        )
+        self.uniq_msg_id.append(msg.message_id)
 
     async def menu_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """Sends a message with three inline buttons attached."""
@@ -133,11 +286,11 @@ class Telegram:
             chat_id=self.chat_id, message_id=update.message.message_id
         )
         msg = await update.message.reply_text(
-            "Please choose:", reply_markup=self.menu_reply_markup
+            "Please choose:", reply_markup=self.reply_markup["menu"]
         )
         self.uniq_msg_id.append(msg.message_id)
 
-    async def fiat_method(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def fiat_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """If received CheckBalance Mode do this"""
         query = update.callback_query
 
@@ -170,11 +323,39 @@ class Telegram:
                 + f"\nMargin : {round(fiat_balance['USDT']['used'],2)}$"
                 + f"\nTotal  : {round(fiat_balance['USDT']['total'],2)}$"
             )
-        elif callback["Method"] == "BACK":
-            msg = "Please choose:"
         msgs = await query.edit_message_text(
-            text=msg, reply_markup=self.menu_reply_markup
+            text=msg, reply_markup=self.reply_markup["menu"]
         )
+        self.uniq_msg_id.append(msgs.message_id)
+
+    async def trade_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """If received CheckBalance Mode do this"""
+        pass
+
+    async def analyse_handler(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        query = update.callback_query
+        await query.answer()
+        callback = eval(query.data)
+        if callback["Method"] == "VXMA":
+            msg = "Please choose:"
+            msgs = await query.edit_message_text(
+                text=msg, reply_markup=self.reply_markup["menu"]
+            )
+        self.uniq_msg_id.append(msgs.message_id)
+
+    async def back_to_menu(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        query = update.callback_query
+        await query.answer()
+        callback = eval(query.data)
+        if callback["Method"] == "BACK":
+            msg = "Please choose:"
+            msgs = await query.edit_message_text(
+                text=msg, reply_markup=self.reply_markup["menu"]
+            )
         self.uniq_msg_id.append(msgs.message_id)
 
     async def button_menu(
@@ -186,48 +367,60 @@ class Telegram:
         await query.answer()
         callback = eval(query.data)
         ## Main menu will be here
-        if callback["Mode"] == "menu":
-            if callback["Method"] == "CheckBalance":
-                msgs = await query.edit_message_text(
-                    text="Please Select Fiat Balance",
-                    reply_markup=self.fiat_reply_markup,
-                )
-                self.uniq_msg_id.append(msgs.message_id)
-            elif callback["Method"] == "Trade":
-                pass
-            elif callback["Method"] == "Analyser":
-                pass
-            elif callback["Method"] == "PositionData":
-                pass
-            elif callback["Method"] == "BotSetting":
-                pass
-            elif callback["Method"] == "apiSetting":
-                pass
+        if callback["Method"] == "CheckBalance":
+            msgs = await query.edit_message_text(
+                text="โปรดเลือกกระเป๋าเงินเฟียต",
+                reply_markup=self.reply_markup["fiat"],
+            )
+        # elif callback["Method"] == "Trade":
+        #     msgs = await query.edit_message_text(
+        #         text="Please Select Fiat Balance",
+        #         reply_markup=self.reply_markup["trade"],
+        #     )
+        elif callback["Method"] == "Analyser":
+            msgs = await query.edit_message_text(
+                text="โปรดเลือกกลยุทธ์ของท่าน",
+                reply_markup=self.reply_markup["analyse"],
+            )
+        elif callback["Method"] == "PositionData":
+            msgs = await query.edit_message_text(
+                text="Postion ที่มีการเปิดอยู่\n{position_data}",
+                reply_markup=self.reply_markup["pnl"],
+            )
+        elif callback["Method"] == "BotSetting":
+            msgs = await query.edit_message_text(
+                text="เหรียญที่ดูอยู่ : {watchlist}\n\nโปรดเลือกการตั้งค่า",
+                reply_markup=self.reply_markup["setting"],
+            )
+        elif callback["Method"] == "apiSetting":
+            msgs = await query.edit_message_text(
+                text="โปรดเลือกการตั้งค่า",
+                reply_markup=self.reply_markup["secure"],
+            )
 
-        ## after that each method call different functions
-        # elif callback["Mode"] == "fiat":
-        #     await self.fiat_method(callback, query)
-        # else select again
         else:
             msgs = await query.edit_message_text(
-                text="Selected again!", reply_markup=self.menu_reply_markup
+                text="Selected again!", reply_markup=self.reply_markup["menu"]
             )
-            self.uniq_msg_id.append(msgs.message_id)
+        # Save message_id to delete at the end.
+        self.uniq_msg_id.append(msgs.message_id)
 
-        if len(self.msg_id) > 0:
-            for id in self.msg_id:
-                try:
-                    await context.bot.delete_message(
-                        chat_id=self.chat_id, message_id=id
-                    )
-                    self.msg_id.remove(id)
-                except Exception:
-                    continue
+    async def clear_task(self):
+        while True:
+            if len(self.msg_id) > 0:
+                for id in self.msg_id:
+                    try:
+                        await self.application.bot.delete_message(
+                            chat_id=self.chat_id, message_id=id
+                        )
+                        self.msg_id.remove(id)
+                    except Exception:
+                        continue
+            await asyncio.sleep(1)
 
     async def clear_command(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> None:
-        """Parses the CallbackQuery and updates the message text."""
         self.msg_id.append(update.message.message_id)
         delete_list = self.uniq_msg_id + self.msg_id
         if len(delete_list) > 0:
